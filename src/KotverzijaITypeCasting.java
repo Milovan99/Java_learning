3639b7c55136473023035c4b287d4476ceeb6933
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-09 21:42:52 +0100

    Konverzija i Type Casting

---
+++ src/KotverzijaITypeCasting.java
@@ -0,0 +1,17 @@
+public class KotverzijaITypeCasting {
+    public static void main(String[] args) {
+
+        byte x;
+        int i =10;
+        double b;
+        double dob= 138.8;
+        b=i;
+        System.out.println(b);
+        x=(byte) i;
+        System.out.println(x);
+
+        int integer=(int) dob;
+        System.out.println(integer);
+
+    }
+}
