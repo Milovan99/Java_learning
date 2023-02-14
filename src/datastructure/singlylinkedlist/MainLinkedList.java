83e90de490299702eb586b8c939685ecb0f9b884
Milovan99 <milovanjakovljevic78@gmail.com>
2023-02-14 15:34:28 +0100

    Creating Linked List

---
+++ src/datastructure/singlylinkedlist/MainLinkedList.java
@@ -0,0 +1,22 @@
+package datastructure.singlylinkedlist;
+
+public class MainLinkedList {
+    public static void main(String[] args) {
+
+    }
+}
+
+class LinkedList{
+    Node head;
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int val){
+            data=val;
+            next=null;
+        }
+    }
+}
+
